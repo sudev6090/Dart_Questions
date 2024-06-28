4b35de33329791df1ae4de808dda52ef8cac49d2
sudev6090 <sudevdevaraj01@gmail.com>
2024-06-28 11:37:26 +0530

    update

---
+++ Leetcode/two_sum.dart
@@ -0,0 +1,17 @@
+class Solution {
+  List<int?> twoSum(List<int> nums, int target) {
+    Map<int,int> numIndices={};
+
+    for(int i = 0; i< nums.length; i++) {
+        int complement = target - nums[i];
+
+        if(numIndices.containsKey(complement)) {
+            return [numIndices[complement], i];
+        }
+        numIndices[nums[i]] = i;
+    }
+    return [];
+  }
+}
+
+
